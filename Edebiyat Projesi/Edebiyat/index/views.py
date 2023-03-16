30c2a30018af4f76bd356cb6a15ab73f33dc4899
PlatinMavi <92332324+PlatinMavi@users.noreply.github.com>
2023-03-16 23:09:46 +0300

    Add files via upload

--- Edebiyat Projesi/Edebiyat/index/views.py
+++ Edebiyat Projesi/Edebiyat/index/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Yazar,Kitap,Dergi,Siir
+import datetime
 
 # Create your views here.
 def index(request):
@@ -11,12 +12,17 @@ def index(request):
     yazar = Yazar.objects.all()
     siir = Siir.objects.all()
 
-
+    zam = datetime.datetime.now()
+    zam = zam.strftime("%m")
+    aylar = ["Ocak","Şubat","Mart","Nisan","Mayıs","Haziran","Temmuz","Ağustos","Eylül","Ekim","Kasım","Aralık"]
+    buay = aylar[int(zam)+1]
+    
     template = loader.get_template("index/index.html")
     context={
         "DERGI":dergi,
         "KITAP":kitap,
         "YAZAR":yazar,
         "SIIR":siir,
+        "AY":buay
     }
     return HttpResponse(template.render(context, request))
